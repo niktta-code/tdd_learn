8f19dbaf2462d169b77d7bead115d63d8b08e30a
nikita <niktta@protonmail.com>
2021-04-29 17:33:33 +0300

    FT front_1

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Эдит открывает домашнюю страницу и случайно пытается отправить
         # пустой элемент списка. Она нажимает Enter на пустом поле ввода
@@ -47,7 +50,30 @@ class ItemValidationTest(FunctionalTest):
 
         # Она видит полезное сообщение об ошибке
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
         ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Эдит начинает список и вызывает ошибку валидации:
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # Она начинает набирать в поле ввода, чтобы очистить ошибку
+        self.get_item_input_box().send_keys('a')
+
+        # Она довольна от того, что сообщение об ошибке исчезает
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
+
+
 
